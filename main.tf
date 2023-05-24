c402a40d8a312f9a39bd397c38f01b43f646a09f
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-05-24 21:42:39 +0530

    update

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+module "vault" {
+  source = "terraform-aws-modules/vault/aws"
+
+  vpc_id     = "vpc-049df61146f12"
+  subnet_ids = ["subnet-049df61146f12"]
+
+  instances_count = 1
+  instance_type   = "t2.medium"
+
+  cluster_name = "vault"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
