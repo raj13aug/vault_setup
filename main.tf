0317e7596fd09d3ecbe33472ceaef9f7a8e2a58c
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-05-25 19:57:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,62 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "vault" {
-  source  = "hashicorp/vault/aws"
-  version = "0.17.0"
+data "aws_ami" "linux" {
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0*"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
 
-  vault_instance_type = "t2.medium"
+resource "aws_instance" "linux" {
+  ami                    = data.aws_ami.linux.image_id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-id1", "sg-id2", "sg-id3"]
+  subnet_id              = "subnet-id"
+  key_name               = "new-eks"
+  tags = {
+    Name = "instance_name"
+  }
+  user_data = <<EOF
+        #!/bin/bash
+        yum update -y
+        yum -y install iscsi-initiator-utils
+        yum -y install lsscsi
+        yum -y install device-mapper-multipath
+        service iscsid start
+        amazon-linux-extras install epel -y
+        yum install sshpass -y
+        iqn=`awk -F= '{ print $2 }' /etc/iscsi/initiatorname.iscsi`
+        sshpass -p pureuser ssh  -oStrictHostKeyChecking=no pureuser@<ctmgmt-vip>> purehost create <hostnameforpure> --iqnlist $iqn
+        sshpass -p pureuser ssh  -oStrictHostKeyChecking=no pureuser@<ctmgmt-vip> purehost connect --vol <purevolname> <hostnameforpure>
+        iscsiadm -m iface -I iscsi0 -o new
+        iscsiadm -m iface -I iscsi1 -o new
+        iscsiadm -m iface -I iscsi2 -o new
+        iscsiadm -m iface -I iscsi3 -o new
+        iscsiadm -m discovery -t st -p <ct0-iscsi-ip>:3260
+        iscsiadm -m node -p <ct0-iscsi-ip> --login
+        iscsiadm -m node -p <ct1-iscsi-ip> --login
+        iscsiadm -m node -L automatic
+        mpathconf --enable --with_multipathd y
+        service multipathd restart
+        mkdir /mnt/cbsvol
+        disk=`multipath -ll|awk '{print $1;exit}'`
+        mount /dev/mapper/$disk /mnt/cbsvol
+        EOF
+}
 
-  vault_cluster_name = "vault"
+output "public_dns" {
+  value = aws_instance.linux.*.public_dns
+}
+output "public_ip" {
+  value = aws_instance.linux.*.public_ip
+}
+output "name" {
+  value = aws_instance.linux.*.tags.Name
 }
