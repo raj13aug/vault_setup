90e6894ddc82268f3c52374093f38aff6562052a
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-05-25 20:27:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,42 +22,30 @@ resource "aws_instance" "linux" {
   user_data = <<EOF
         #!/bin/bash
         set -e
-        echo "Installing dependencies..."
-        sudo apt-get update -y
-        sudo apt-get install -y unzip
-
-
-        echo "Fetching vault..."
-        VAULT=0.6.5
-        cd /tmp
-        wget https://releases.hashicorp.com/vault/${VAULT}/vault_${VAULT}_linux_amd64.zip -O vault.zip
-        wget https://gist.githubusercontent.com/ewilde/b3f622e0899b5188263238c2e54054c9/raw/8f7816ed98f4f4cc9efa5034a2ce300e0e8f95d7/vault.service -O vault.service
-
-        echo "Installing Vault..."
-        unzip vault.zip >/dev/null
-        chmod +x vault
-        sudo mv vault /usr/local/bin/vault
-
-       # Write the flags to a temporary file
-        cat >/tmp/vault_flags << EOF
-        OPTIONS=""
-        KEYS="your keys after"
-        VAULT_TOKEN="your root token"
-        VAULT_ADDR="http://0.0.0.0:8200"
-
-
-        echo "Installing Systemd service..."
-        sudo mkdir -p /etc/vault.d
-        sudo tee -a /etc/vault.d/vault.conf >/dev/null <<'EOF'
+        curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+        apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+        apt update
+        apt install vault -y
         
-        sudo chown root:root /tmp/vault.service
-        sudo mv /tmp/vault.service /etc/systemd/system/vault.service
-        sudo chmod 0644 /etc/systemd/system/vault.service
-        sudo mv /tmp/vault_flags /etc/default/vault
-        sudo chown root:root /etc/default/consul
-        sudo chmod 0644 /etc/default/consul
+        cat /dev/null > /etc/vault.d/vault.conf
+        
+        sudo tee -a /etc/vault.d/vault.conf >/dev/null 
+        ui = true
 
-      EOF
+        storage "file" {
+        path = "/opt/vault/data"
+        }
+
+        # HTTPS listener
+        listener "tcp" {
+        address = "0.0.0.0:8200"
+        tls_cert_file = "/opt/vault/tls/tls.crt"
+        tls_key_file = "/opt/vault/tls/tls.key"
+        }
+
+       systemctl start vault
+       systemctl status vault
+EOF
 }
 
 
