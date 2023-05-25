dbffba7ab77cf3c3ca68ed73ad7b3ea4b8616369
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-05-25 19:58:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,29 +26,7 @@ resource "aws_instance" "linux" {
   }
   user_data = <<EOF
         #!/bin/bash
-        yum update -y
-        yum -y install iscsi-initiator-utils
-        yum -y install lsscsi
-        yum -y install device-mapper-multipath
-        service iscsid start
-        amazon-linux-extras install epel -y
-        yum install sshpass -y
-        iqn=`awk -F= '{ print $2 }' /etc/iscsi/initiatorname.iscsi`
-        sshpass -p pureuser ssh  -oStrictHostKeyChecking=no pureuser@<ctmgmt-vip>> purehost create <hostnameforpure> --iqnlist $iqn
-        sshpass -p pureuser ssh  -oStrictHostKeyChecking=no pureuser@<ctmgmt-vip> purehost connect --vol <purevolname> <hostnameforpure>
-        iscsiadm -m iface -I iscsi0 -o new
-        iscsiadm -m iface -I iscsi1 -o new
-        iscsiadm -m iface -I iscsi2 -o new
-        iscsiadm -m iface -I iscsi3 -o new
-        iscsiadm -m discovery -t st -p <ct0-iscsi-ip>:3260
-        iscsiadm -m node -p <ct0-iscsi-ip> --login
-        iscsiadm -m node -p <ct1-iscsi-ip> --login
-        iscsiadm -m node -L automatic
-        mpathconf --enable --with_multipathd y
-        service multipathd restart
-        mkdir /mnt/cbsvol
-        disk=`multipath -ll|awk '{print $1;exit}'`
-        mount /dev/mapper/$disk /mnt/cbsvol
+
         EOF
 }
 
