0e37201cb9175824dc8a0a36bf521f6d80cdf22d
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-05-26 12:44:35 +0530

    Update vault_script.sh

--- vault_script.sh
+++ vault_script.sh
@@ -4,3 +4,27 @@ curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 apt update
 apt install vault -y
+
+
+tee /etc/vault.d/vault.hcl <<EOF
+ui = true
+
+#mlock = true
+#disable_mlock = true
+
+storage "file" {
+path = "/opt/vault/data"
+}
+
+
+# HTTPS listener
+listener "tcp" {
+address = "0.0.0.0:8200"
+tls_cert_file = "/opt/vault/tls/tls.crt"
+tls_key_file = "/opt/vault/tls/tls.key"
+}
+EOF
+
+systemctl start vault
+systemctl status vault
+systemctl enable vault
