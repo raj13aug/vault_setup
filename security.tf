0a55e1f62fb1bd08d236f015936906030995217e
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-05-29 15:03:09 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -18,3 +18,25 @@ resource "aws_security_group" "ec2" {
     Name = "allow_vault"
   }
 }
+
+
+resource "aws_security_group" "ec2" {
+  name        = "allow_vault_ssh"
+  description = "Allow vault ssh outbound traffic"
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "allow_vault_ssh"
+  }
+}
