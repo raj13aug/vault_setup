31b0e71f5b0120a3a5a17da5d1d570e724708d47
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2023-06-02 09:46:54 +0530

    Update vault_script.sh

--- vault_script.sh
+++ vault_script.sh
@@ -32,8 +32,7 @@ max_lease_ttl         = "10h"
 default_lease_ttl    = "10h"
 cluster_name         = "vault"
 raw_storage_endpoint     = true
-disable_sealwrap     = true
-disable_printable_check = true
+
 EOF
 
 # Enable the Vault service
